1dc708ce88ee50c6b2229383d05a9f8c0150ac99
Rien van Velzen <rien@dailydialogues.nl>
2015-12-23 16:48:44 +0100

    Initial commit

---
+++ jokefactory/src/main/java/com/example/JokeFactory.java
@@ -0,0 +1,9 @@
+package com.example;
+
+public class JokeFactory {
+
+    public String getJoke() {
+        return "A jumper cable walks into a bar, the bartender says 'I will serve you, but don't start anything!'.";
+    }
+
+}
